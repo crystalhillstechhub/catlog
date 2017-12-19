91f5c16fecc6f736f384de5ec9bb65a029659bec
akintola olalekan <akinlekan@gmail.com>
2017-12-19 13:19:56 +0100

    Added bootstrap cdn

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         <!--CSS-->
         
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
         <!--Google Fonts-->
