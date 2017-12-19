cdcaad073c1e40ce529da88b44fd08ac725c4ce0
akintola olalekan <akinlekan@gmail.com>
2017-12-19 13:28:05 +0100

    Removed a few things

--- index.php
+++ index.php
@@ -148,8 +148,7 @@
        
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0/popper.min.js"></script>
+       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
         <script>new WOW().init();</script>
 
