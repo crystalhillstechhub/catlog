6377dabb0b6ea5bfc3e640b06823076c685a60dc
Ajilore Raphael Olamide <raphealolams@gmail.com>
2017-12-19 12:00:15 +0100

    php file added

---
+++ connection.php
@@ -0,0 +1,18 @@
+<?php
+/* Database config */
+$db_host		= 'localhost';
+$db_user		= 'root';
+$db_pass		= '';
+$db_database	= 'food_concept'; 
+
+/* End config */
+
+try{
+    $db = new PDO('mysql:host='.$db_host.';dbname='.$db_database, $db_user, $db_pass);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+}catch(PDOException $e){
+    echo "Connection failed: " . $e->getMessage();
+}
+
+?>
